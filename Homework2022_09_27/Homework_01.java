28fd1ff087dc1bf4845b8748b659ed0cca2cf043
Vladyslav Rudnytskyi <vlad.alt1997@gmail.com>
2022-09-29 11:12:18 +0300

    2022 09 27

--- Homework2022_09_27/Homework_01.java
+++ Homework2022_09_27/Homework_01.java
@@ -4,19 +4,15 @@ package Homework2022_09_27;
 
 public class Homework_01 {
     public static void main(String[] args) {
-        int num1 = -3;
+        int num1 = 11;
         int num2 = 11;
         printOddNumbers(num1,num2);
     }
 
     public static void printOddNumbers(int num1, int num2) {
-        if (num1 == num2 && num1 % 2 != 0) {
-            System.out.println(num1);
-        } else {
-            for (int i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
-                if (i % 2 != 0) {
-                    System.out.println(i);
-                }
+        for (int i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
             }
         }
     }
